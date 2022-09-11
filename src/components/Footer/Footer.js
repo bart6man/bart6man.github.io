9494f54b3ea3019c32feceb6b983b922f08df198
bart6man <igor.musson.55@gmail.com>
2022-09-11 15:02:12 +0300

    ADAPRIVE

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,10 @@ export default function Footer() {
         <div className="footer">
           <div className="content-container">
             <div className="footer-container">
-              <div className="footer-text">Kyiv, Ukraine</div>
-              <div className="footer-info">bartybart.6man@gmail.com</div>
-              <Up />
+              <div className="footer-item">Kyiv, Ukraine</div>
+              <div className="footer-item">bartybart.6man@gmail.com</div>
+              <div className="footer-item"> <Up /></div>
+             
             </div>
           </div>
         </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,46 +3,89 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <div className="pray">
-        Help Ukrainian soldiers return home alive
-        <a
-          className="navbar-link "
-          href="https://savelife.in.ua/donate/#donate-army-card-monthly"
-          target="_blank"
-          rel="noreferrer"
-        >
-          ❤️
-        </a>
+      <div className="header_drop">
+      <div className="logo_drop">
+      
+      {" "}
+      <img src="../6man.png" alt="logo" width={150} height={100} />{" "}
       </div>
-      <div className="header">
-        <div className="logo">
-          <a href="/">
-            {" "}
-            <img src="../6man.png" alt="logo" width={150} height={100} />{" "}
-          </a>
-        </div>
-        <ul className="navbar d-flex ">
-          <li className=" mr-40">
-            <Link to="/" className="navbar-link">
+      <div className="main_drop">
+          <ul className="drop_bar">
+          <li className="drop-bar_text mr-40">
+            <Link to="/" className="drop_bar-link">
               <h3>HOME</h3>
             </Link>
           </li>
           <li className=" mr-40">
-            <Link to="/songs" className="navbar-link">
+            <Link to="/songs" className="drop_bar-link">
               <h3>SONGS</h3>
             </Link>
           </li>
           <li className=" mr-40">
-            <Link to="/beats" className="navbar-link">
+            <Link to="/beats" className="drop_bar-link">
               <h3>BEATS</h3>
             </Link>
           </li>
           <li>
-            <Link to="/contacts" className="navbar-link">
+            <Link to="/contacts" className="drop_bar-link">
               <h3>CONTACTS</h3>
             </Link>
           </li>
+          <li>
+          <a
+            className="navbar-link "
+            href="https://savelife.in.ua/donate/#donate-army-card-monthly"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ❤️
+          </a>
+          </li>
         </ul>
+        </div>
+      </div>
+      <div className="header_main">
+        <div className="pray">
+          Help Ukrainian soldiers return home alive
+          <a
+            className="navbar-link "
+            href="https://savelife.in.ua/donate/#donate-army-card-monthly"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ❤️
+          </a>
+        </div>
+        <div className="header">
+          <div className="logo">
+            <a href="/">
+              {" "}
+              <img src="../6man.png" alt="logo" width={150} height={100} />{" "}
+            </a>
+          </div>
+          <ul className="navbar d-flex ">
+            <li className=" mr-40">
+              <Link to="/" className="navbar-link">
+                <h3>HOME</h3>
+              </Link>
+            </li>
+            <li className=" mr-40">
+              <Link to="/songs" className="navbar-link">
+                <h3>SONGS</h3>
+              </Link>
+            </li>
+            <li className=" mr-40">
+              <Link to="/beats" className="navbar-link">
+                <h3>BEATS</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacts" className="navbar-link">
+                <h3>CONTACTS</h3>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
